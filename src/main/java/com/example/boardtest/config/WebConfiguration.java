365d10608ec056373d130066bbb3c38e5c23fd6e
postwo <codo7717@naver.com>
2024-07-04 22:42:01 +0900

    csrf,cors 적용 완료 postman Basic auth 에서 설정한 비번 이름 등 꼭 맞춰주기 들리면 401에러 발생

--- src/main/java/com/example/boardtest/config/WebConfiguration.java
+++ src/main/java/com/example/boardtest/config/WebConfiguration.java
@@ -8,16 +8,37 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class WebConfiguration  {
 
+    //cors bean 설정
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000","http://127.0.0.1:3000"));
+        configuration.setAllowedMethods(List.of("GET","POST","PATCH","DELETE"));
+        configuration.setAllowedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/v1/**",configuration);
+        return source;
+    }
+
+
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((auth) -> auth.anyRequest().authenticated())
+        http.cors(Customizer.withDefaults()) //cors 활성화
+                .authorizeHttpRequests((requests) -> requests.anyRequest().authenticated())
                 .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션이 생성되지 않게 설정
                 .csrf(CsrfConfigurer::disable) // csrf는 제외
                 .httpBasic(Customizer.withDefaults());
         return http.build();
     }
+
+
 }
